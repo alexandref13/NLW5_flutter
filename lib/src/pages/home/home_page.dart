49006cc00df1b821d17397e78c263f5099629ea5
alexandref13 <alexandrefilhoalvo@gmail.com>
2021-04-19 14:23:54 -0300

    Aula 01

---
+++ lib/src/pages/home/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:dev_quiz/src/pages/home/widgets/appBar/app_bar_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(),
+    );
+  }
+}
---
+++ lib/src/pages/home/widgets/scoreCard/score_card_widget.dart
@@ -0,0 +1,46 @@
+import 'package:dev_quiz/core/app_colors.dart';
+import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/src/pages/home/widgets/chart/chart_widget.dart';
+import 'package:flutter/material.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        height: 136,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(flex: 1, child: ChartWidget()),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: EdgeInsets.only(left: 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Vamos começar!',
+                      style: AppTextStyles.heading,
+                    ),
+                    Text(
+                      'Complete os desafios e avance em conhecimento',
+                      style: AppTextStyles.body,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
